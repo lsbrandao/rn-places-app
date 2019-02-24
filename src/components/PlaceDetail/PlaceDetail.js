c895f002671e1ab8f3d0ad068c57176308e3dcde
Luiz <luiz.sb@live.com>
2019-02-24 18:14:54 -0500

    Added Redux

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -3,30 +3,31 @@ import { Modal, View, Image, Text, Button, StyleSheet } from 'react-native';
 
 const PlaceDetail = props => {
 	let modalContent = null;
-
+  
 	if (props.selectedPlace) {
-		modalContent = (
-			<View>
-				<Image source={props.selectedPlace.image} style={styles.placeImage} />
-				<Text style={styles.placeName}>{props.selectedPlace.name}</Text>
-			</View>
-		);
+	  modalContent = (
+		<View>
+		  <Image source={props.selectedPlace.image} style={styles.placeImage} />
+		  <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+		</View>
+	  );
 	}
 	return (
-		<Modal
-			onRequestClose={props.onModalClosed}
-			visible={props.selectedPlace !== null}
-			animationType="slide">
-		<View>
-			<View style={styles.modalContainer}>
-				{modalContent}
-					<Button title="Delete" color="red" onPress={props.onItemDeleted}/>
-					<Button title="Close" onPress={props.onModalClosed} />
-			</View>
+	  <Modal
+		onRequestClose={props.onModalClosed}
+		visible={props.selectedPlace !== null}
+		animationType="slide"
+	  >
+		<View style={styles.modalContainer}>
+		  {modalContent}
+		  <View>
+			<Button title="Delete" color="red" onPress={props.onItemDeleted} />
+			<Button title="Close" onPress={props.onModalClosed} />
+		  </View>
 		</View>
-		</Modal >
+	  </Modal>
 	);
-};
+  };
 
 const styles = StyleSheet.create({
 	modalContainer: {
