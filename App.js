d1ea16b85583980ff31c1947ad3c863115733317
Luiz <luiz.sb@live.com>
2019-02-24 14:37:29 -0500

    Added PlaceDetail component

--- App.js
+++ App.js
@@ -3,26 +3,78 @@ import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList.js/PlaceList';
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+
+import placeImage from './src/assets/Elevador-Lacerda-Salvador-Bahia.jpg';
+import torontoImage from './src/assets/toronto.jpg';
 
 
 export default class App extends Component {
   state = {
-    places: ['Munich', 'Toronto']
+    places: [
+      {
+        key: Math.random().toString(),
+        name: 'Toronto',
+        image: torontoImage
+      }
+    ],
+    selectedPlace : null
   };
 
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random().toString(),
+          name: placeName,
+          image: {
+            uri: 'https://s28481.pcdn.co/wp-content/uploads/2018/05/Siquijor-Beautiful-Places-in-the-Philippines.jpg'
+          }
+        })
       };
     });
   };
 
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+        })
+      };
+    });
+  };
+
+  placeDeletedHandler = () => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place) => {
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    });
+  }
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <PlaceDetail
+          selectedPlace={this.state.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} />
+        <PlaceList
+          places={this.state.places}
+          onItemSelected={this.placeSelectedHandler}
+        />
       </View>
     );
   }
